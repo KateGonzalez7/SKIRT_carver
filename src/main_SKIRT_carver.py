431d954ed64d5e6dba1ee3b641ad04768e89df8a
Kate Gonzalez <kate.gonzalez3572@gmail.com>
2024-08-15 07:08:13 +0000

    Updated files

--- src/main_SKIRT_carver.py
+++ src/main_SKIRT_carver.py
@@ -5,13 +5,12 @@ from writer_SKIRT_carver import SnapshotData
 
 fname = h5py.File('snapshot_200.hdf5', 'r')
 
-getSnapInfo(fname)
-
 snapshot = SnapshotData(fname)
-snapshot.gasFile(fname), snapshot.sourceFile(fname), snapshot.gasSkirt(fname), snapshot.sourceSkirt(fname)
-gas_data = snapshot.gas_data
-gas_skirt = snapshot.gas_skirt
-src_data = snapshot.source_data
-src_skirt = snapshot.source_skirt
+snapshot.gasFile(fname), snapshot.sourceFile(fname)
 
-createSki(fname)
+gasFile = snapshot.gasSkirt(fname)
+srcFile = snapshot.sourceSkirt(fname)
+skiFile = createSki(fname, gasFile, srcFile)
+
+skirt = sm.Skirt()
+sim = skirt.execute(f"{skiFile}", console='brief')
